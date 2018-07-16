e64af070d322c04f63f08e83d5f4468a68370fd1
Kahlia Lepple <klepple@uwo.ca>
2018-07-16 16:41:22 -0400

    initial commit

---
+++ personal-site/src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from "typography";
+//import bootstrapTheme from "typography-theme-bootstrap";
+import fairyTheme from "typography-theme-fairy-gates";
+
+const typography = new Typography(fairyTheme);
+
+export default typography;
