c156d12647f21872d73729c84268445a9ce9f4a6
Kahlia Lepple <klepple@uwo.ca>
2018-07-26 17:00:15 -0400

    Added utils

--- personal-site/src/utils/typography.js
+++ personal-site/src/utils/typography.js
@@ -1,7 +1,7 @@
 import Typography from "typography";
-//import bootstrapTheme from "typography-theme-bootstrap";
-import fairyTheme from "typography-theme-fairy-gates";
+import bootstrapTheme from "typography-theme-bootstrap";
+import judahTheme from "typography-theme-judah";
 
-const typography = new Typography(fairyTheme);
+const typography = new Typography(judahTheme);
 
 export default typography;
